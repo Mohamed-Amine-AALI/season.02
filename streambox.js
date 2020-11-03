3eb3d6a4993ba3bb8930eae7c50b30c87d72277f
AMINE.AALI <ma.aali@linkbynet.com>
2020-11-03 16:09:25 +0100

    last

--- streambox.js
+++ streambox.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const { cpuUsage } = require('process');
 const { Transform } = require('stream');
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
 
 function duplicate(fileName) {
     const { name, ext } = path.parse(fileName);
@@ -37,18 +38,43 @@ function csv2json(filename) {
     const input = fs.createReadStream(filename)
     const rl = readline.createInterface({ input })
   
+    let headers = []
+    let records = []
+  
     let lineCount = 0
   
-    rl.on('line', (line) => {
+    rl.on('line', (line, i) => {
       if (lineCount === 0) {
-        // Header
-        
+        headers = line.split(';')
       } else {
-        // Data
+        const record = {}
+  
+        line.split(';').forEach((value, i) => {
+          const key = headers[i]
+  
+          if (value.includes(',')) {
+            record[key] = value.split(',')
+          } else {
+            record[key] = value
+          }
+        })
+  
+        records.push(record)
       }
-      
-      console.log(`line(${lineCount}):`, line)
+  
       lineCount++
+    })
+  
+    rl.on('close', () => {
+      const { name } = path.parse(filename)
+  
+      fs.writeFile(`${name}.json`, JSON.stringify(records, null, 2), (err) => {
+        if(err) {
+          return console.log(err);
+        }
+  
+        console.log(`${filename} converted to json: ${name}.json`)
+      })
     })
   }
 
